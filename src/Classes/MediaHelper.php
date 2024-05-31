6cb486b476ab7349b6a21bf0afa9db895d7ae9fc
Robin van Maasakker <robin@qubiqx.com>
2024-05-31 11:55:38 +0200

    Update

--- src/Classes/MediaHelper.php
+++ src/Classes/MediaHelper.php
@@ -7,16 +7,17 @@ use RalphJSmit\Filament\MediaLibrary\Forms\Components\MediaPicker;
 
 class MediaHelper extends Command
 {
-    public function field($name = 'image', $label = 'Afbeelding', $required = false, $multiple = false, $isImage = false): MediaPicker
+    public function field($name = 'image', $label = 'Afbeelding', $required = false, $multiple = false, $isImage = false)
     {
 
-        $mediaPicker = MediaPicker::make($name)
-            ->label($label)
-            ->required($required)
-            ->multiple($multiple)
-            ->showFileName()
-            ->downloadable()
-            ->reorderable();
+        return;
+//        $mediaPicker = MediaPicker::make($name)
+//            ->label($label)
+//            ->required($required)
+//            ->multiple($multiple)
+//            ->showFileName()
+//            ->downloadable()
+//            ->reorderable();
 
         if($isImage) {
             $mediaPicker->acceptedFileTypes(['image/*']);
