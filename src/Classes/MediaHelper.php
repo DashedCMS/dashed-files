a15f6027ab4c306e051ef9d98249235dc1b35a65
Robin van Maasakker <robin@qubiqx.com>
2024-05-31 13:29:05 +0200

    Update

--- src/Classes/MediaHelper.php
+++ src/Classes/MediaHelper.php
@@ -3,13 +3,13 @@
 namespace Dashed\DashedFiles\Classes;
 
 use Illuminate\Console\Command;
+use RalphJSmit\Filament\MediaLibrary\FilamentMediaLibrary;
 use RalphJSmit\Filament\MediaLibrary\Forms\Components\MediaPicker;
 
 class MediaHelper extends Command
 {
     public function field($name = 'image', $label = 'Afbeelding', $required = false, $multiple = false, $isImage = false): MediaPicker
     {
-
         $mediaPicker = MediaPicker::make($name)
             ->label($label)
             ->required($required)
@@ -18,10 +18,23 @@ class MediaHelper extends Command
             ->downloadable()
             ->reorderable();
 
-        if($isImage) {
+        if ($isImage) {
             $mediaPicker->acceptedFileTypes(['image/*']);
         }
 
         return $mediaPicker;
+    }
+
+    public function plugin()
+    {
+        return FilamentMediaLibrary::make()
+            ->navigationGroup('Media')
+            ->navigationSort(1)
+            ->navigationLabel('Media Browser')
+            ->navigationIcon('heroicon-o-camera')
+            ->activeNavigationIcon('heroicon-s-camera')
+            ->pageTitle('Media Browser')
+            ->acceptPdf()
+            ->slug('media-browser');
     }
 }
