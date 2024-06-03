5f19bd73e1b39a5a3ac4e276153b88d1fc31ee0e
Robin van Maasakker <robin@qubiqx.com>
2024-06-03 16:36:14 +0200

    Update

--- src/Classes/MediaHelper.php
+++ src/Classes/MediaHelper.php
@@ -3,8 +3,12 @@
 namespace Dashed\DashedFiles\Classes;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use RalphJSmit\Filament\MediaLibrary\FilamentMediaLibrary;
 use RalphJSmit\Filament\MediaLibrary\Forms\Components\MediaPicker;
+use RalphJSmit\Filament\MediaLibrary\Media\DataTransferObjects\MediaItemMeta;
+use RalphJSmit\Filament\MediaLibrary\Media\Models\MediaLibraryItem;
 
 class MediaHelper extends Command
 {
@@ -28,13 +32,64 @@ class MediaHelper extends Command
     public function plugin()
     {
         return FilamentMediaLibrary::make()
-            ->navigationGroup('Media')
+            ->navigationGroup('Content')
             ->navigationSort(1)
             ->navigationLabel('Media Browser')
             ->navigationIcon('heroicon-o-camera')
             ->activeNavigationIcon('heroicon-s-camera')
             ->pageTitle('Media Browser')
             ->acceptPdf()
+            ->acceptVideo()
+            ->conversionMedium(enabled: false)
+            ->conversionSmall(enabled: false)
+            ->conversionThumb(enabled: true, width: 600, height: 600)
+            ->firstAvailableUrlConversions([
+                'thumb',
+            ])
             ->slug('media-browser');
+    }
+
+    public function getSingleImage(int|string|array $mediaId, string $conversion = 'thumb'): ?MediaItemMeta
+    {
+        if (is_string($mediaId) && filter_var($mediaId, FILTER_VALIDATE_INT) === false) {
+            return null;
+        }
+
+        if (is_array($mediaId)) {
+            $mediaId = $mediaId[0];
+        }
+
+        if (!is_int($mediaId)) {
+            $mediaId = (int)$mediaId;
+        }
+
+        $media = Cache::rememberForever('media-library-media-' . $mediaId, function () use ($mediaId, $conversion) {
+            $media = MediaLibraryItem::find($mediaId);
+            $meta = $media->getMeta();
+            $mediaItem = $media->getItem();
+            $meta->url = $mediaItem->getAvailableUrl([$conversion]);
+            return $meta;
+        });
+
+        return $media;
+    }
+
+    public function getMultipleImages(array $mediaIds, string $conversion = 'thumb'): ?Collection
+    {
+        if (is_string($mediaIds)) {
+            return null;
+        }
+
+        if (is_int($mediaIds)) {
+            return null;
+        }
+
+        $medias = [];
+
+        foreach($mediaIds as $id){
+            $medias[] = $this->getSingleImage($id, $conversion);
+        }
+
+        return collect($medias);
     }
 }
--- src/DashedFilesServiceProvider.php
+++ src/DashedFilesServiceProvider.php
@@ -2,18 +2,46 @@
 
 namespace Dashed\DashedFiles;
 
+use Closure;
 use Dashed\DashedFiles\Commands\MigrateFilesToSpatieMediaLibrary;
 use Dashed\DashedFiles\Commands\MigrateImagesInDatabase;
+use RalphJSmit\Filament\MediaLibrary\Facades\MediaLibrary;
+use RalphJSmit\Filament\MediaLibrary\Media\DataTransferObjects\MediaItemMeta;
+use RalphJSmit\Filament\MediaLibrary\Media\Models\MediaLibraryItem;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class DashedFilesServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'dashed-files';
 
+    public function bootingPackage()
+    {
+        MediaLibrary::registerMediaConversions(function (MediaLibraryItem $mediaLibraryItem, Media $media = null) {
+            $mediaLibraryItem
+                ->addMediaConversion('huge')
+                ->width(1600);
+            $mediaLibraryItem
+                ->addMediaConversion('large')
+                ->width(1200);
+            $mediaLibraryItem
+                ->addMediaConversion('medium')
+                ->width(800);
+            $mediaLibraryItem
+                ->addMediaConversion('small')
+                ->width(400);
+            $mediaLibraryItem
+                ->addMediaConversion('tiny')
+                ->width(200);
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dashed-files');
 
         $package
             ->name('dashed-files')
