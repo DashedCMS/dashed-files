87620a03b74f55e661c6e7226f7e43982a4bf730
Robin van Maasakker <robin@qubiqx.com>
2024-12-09 20:27:24 +0100

    Update

--- resources/views/components/image.blade.php
+++ resources/views/components/image.blade.php
@@ -11,27 +11,29 @@
     'height' => '',
     'width' => '',
 ])
-@php
-    $media = mediaHelper()->getSingleMedia($mediaId, $manipulations ?: $conversion);
-    $url = $media->url ?? '';
-    $alt = $media->altText ?? $alt;
-    $isVideo = $media->isVideo ?? false;
-@endphp
-@if($isVideo)
-    <video {{ $attributes }}
-           @if($controls) controls @endif
-           @if($autoplay) autoplay @endif
-           @if($muted) muted @endif
-           @if($loop) loop @endif
-           loading="{{ $loading }}">
-        <source src="{{ $url }}" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-@else
-    <img
-        src="{{ $url }}"
-        alt="{{ $alt }}"
-        loading="{{ $loading }}"
-        {{ $attributes }}
-    >
+@if($mediaId)
+    @php
+        $media = mediaHelper()->getSingleMedia($mediaId, $manipulations ?: $conversion);
+        $url = $media->url ?? '';
+        $alt = $media->altText ?? $alt;
+        $isVideo = $media->isVideo ?? false;
+    @endphp
+    @if($isVideo)
+        <video {{ $attributes }}
+               @if($controls) controls @endif
+               @if($autoplay) autoplay @endif
+               @if($muted) muted @endif
+               @if($loop) loop @endif
+               loading="{{ $loading }}">
+            <source src="{{ $url }}" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    @else
+        <img
+                src="{{ $url }}"
+                alt="{{ $alt }}"
+                loading="{{ $loading }}"
+                {{ $attributes }}
+        >
+    @endif
 @endif
